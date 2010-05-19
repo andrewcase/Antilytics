9f0b2fce08fb3fab3adc620d05f7dcc8682dcc14
Andrew Case <acase@cims.nyu.edu>
2010-05-19 19:11:30 -0400

    Still setting up the build env.

--- src/chrome/content/browserOverlay.js
+++ src/chrome/content/browserOverlay.js
@@ -1,20 +1,20 @@
 /**
- * XULSchoolChrome namespace.
+ * AntilyticsChrome namespace.
  */
-if ("undefined" == typeof(XULSchoolChrome)) {
-  var XULSchoolChrome = {};
+if ("undefined" == typeof(AntilyticsChrome)) {
+  var AntilyticsChrome = {};
 };
 
 /**
  * Controls the browser overlay for the Hello World extension.
  */
-XULSchoolChrome.BrowserOverlay = {
+AntilyticsChrome.BrowserOverlay = {
   /**
    * Says 'Hello' to the user.
    */
   sayHello : function(aEvent) {
-    let stringBundle = document.getElementById("xulschoolhello-string-bundle");
-    let message = stringBundle.getString("xulschoolhello.greeting.label");
+    let stringBundle = document.getElementById("antilytics-string-bundle");
+    let message = stringBundle.getString("antilytics.greeting.label");
 
     window.alert(message);
   }
